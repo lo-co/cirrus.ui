f1f1a88a9770fd8948bce5dcbf8ab2833f1f196e
lo-co <matt.richardson@msrconsults.com>
2015-12-11 14:47:02 -0700

    Updated the button to use the .input-addon-ctl (or whatever bootstrappy thing it is).  Added some sass files that I will eventually use for styling.

--- ibutton/ibutton.js
+++ ibutton/ibutton.js
@@ -1,19 +1,48 @@
 (function () {
+    'use strict'
+    /**
+     * @ngdoc overview
+     * @name cirrus.ui.ibutton
+     * @description
+     * Provides a common UI element for a button.  The button may contain an indicator for
+     * the current state of the variable associated with the button.
+     */
     angular.module('cirrus.ui.ibutton', [])
-        /* .config('$routeProvider', function($routeProvider){
-             $routeProvider.when('/ibutton')
-         })*/
         .directive('cuiButton', [function () {
+            /**
+             * @ngdoc directive
+             * @name cirrus.ui.ibutton.directive:cuiButton
+             * @scope
+             * @restrict E
+             * @param {number=} width Optional value specifying the width of the span containing the 
+             * indicator in pixels. Default value is 40.
+             * @param {function=} evntFunc Optional function to fire when the button is pressed.
+             * @param {boolean=} info Optional boolean that determines whether indicator is for informational
+             * purposes only or if the indicator is an error.  If the value is false, then the indicator
+             * lit will suggest an error condition and will be red; otherwise, it will be a forest green.
+             * The default is ``true`` (info only).
+             * @param {boolean=} indicate Optional boolean determining whether the button is for 
+             * indicating purposes only.  Default is false.
+             *
+             * @description
+             * This directive provides the DOM definition for a button that 1) has a consistent
+             * look-and-feel and 2) provides feedback regarding the current state of the variable.
+             * The event function should some how tie into a current value table while a data table 
+             * should be used to populate the indicator.
+             * 
+             */
 
-            var cStyle = '{"background-color":iVal? "green":"red"}';
-
-            var template = '<button class="btn btn-block btn-default btn-sm" ng-style="" style="border-style:groove;padding-top:0px;padding-bottom:0px; padding-left:0px; padding-right: 0px;border-radius: 5px;background-color:#777; color:white; border-color:darkgrey;border-width:2px;font-weight: 500;text-transform:uppercase; text-align:left; font-size:100%;">' +
-                '<div style="float:left;height:100%; margin-left:5px;"><span class="badge" style="border-style:inset;height:1em; width:1em;border-color: grey;border-width: 1px; border-radius:0px; top:5px; float:left; margin-right:5px;padding-right: 0px;clear:both; display:inline-block" ></span></div> <div style="padding-top:0px;padding-bottom:0px;padding-left: 5px;display:inline-block;border-left:2px groove darkgrey;width:auto; margin:0;' +
-                'color: ghostwhite"  ng-transclude></div>' +
-                '</button>';
+            var ntemplate = '<div class="input-group" style="margin-top:2px; border-width:0px;width:100%">' +
+                '<span class="input-group-addon" style = "width:16px;border-width:1px;padding:2px 3px 0px 3px; border:1px solid whitesmoke;"><span class="badge" style="border:1px inset grey;height:12px; width:12px;padding:0px;border-width: 1px; border-radius:0px; clear:both;display:inline-block" ></span></span>' +
+                '<button class="btn btn-block btn-default btn-sm" style="margin-left:0;padding:0px 5px;background-color:#777; color:white; text-transform:uppercase;border-radius:0px 5px 5px 0px;border-style:solid; border-color: whitesmoke;border-width:1px;" ng-transclude></button>' +
+                '</div>';
 
             /* Link function used in the DDO returned below...*/
             var link = function (scope, e, attrs) {
+
+                if (scope.indicate === undefined) {
+                    scope.indicate = false;
+                }
 
                 var b = e.find('button');
 
@@ -34,16 +63,14 @@
                         badge.css('background-color', color);
                     }
 
-
                 });
 
+                // Listen for clicks - if no function is attached, do nothing
                 if (scope.evntFunc !== undefined) {
                     e.on('click', function () {
                         scope.evntFunc();
                     });
                 }
-
-
 
             }
             return {
@@ -53,9 +80,10 @@
                     ival: '=',
                     width: '=?',
                     info: '=?',
-                    evntFunc: '&?'
+                    evntFunc: '&?',
+                    indicate: '=?'
                 },
-                template: template,
+                template: ntemplate,
                 link: link
             }
     }]);
